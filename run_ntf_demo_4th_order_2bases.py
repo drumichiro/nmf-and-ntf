fb3fd05de79d82a986ba40636c6b93639c3f7d10
drumichiro <portnoy1207@gmail.com>
2015-07-12 04:06:04 +0100

    Added new use case with 4th order tensor and 2 bases.

---
+++ run_ntf_demo_4th_order_2bases.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+'''
+Created on 2015/07/12
+
+@author: drumichiro
+'''
+from ntf_demo_util import run_ntf_demo
+
+
+if __name__ == '__main__':
+    # Generate samples as input data from Gaussians.
+    mu = [[10, 10, 20, 20],
+          [30, 30, 30, 30]]
+    sigma = [[[5, 0, 0, 0],
+              [0, 5, 0, 0],
+              [0, 0, 5, 0],
+              [0, 0, 0, 5]],
+             [[5, 0, 0, 0],
+              [0, 5, 0, 0],
+              [0, 0, 5, 0],
+              [0, 0, 0, 5]]]
+    eachSampleNum = 100
+    run_ntf_demo(mu, sigma, eachSampleNum)
